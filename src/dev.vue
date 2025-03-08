7bb170f9339e45078b5ee7ea9e44ef85fc2bbd58
Jorge Asus <jorgehn1995@gmail.com>
2025-03-08 12:23:50 -0600

    se agrego el entorno de pruebas

---
+++ src/dev.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { LernizTable } from './index'; // Importa el componente directamente
+</script>
+
+<template>
+  <LernizTable :isLoadingTable="false" :tableID="'test-table'" :zoom="100" :showTable="true">
+    <template #columns>
+      <th v-for="n, i in 15">Name</th>
+
+    </template>
+    <template #rows>
+      <tr v-for="a, i in 30">
+        <td>1</td>
+        <td>John Doe</td>
+        <td v-for="r, i in 14">30</td>
+      </tr>
+    </template>
+  </LernizTable>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,31 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 
-export default defineConfig({
-  plugins: [vue(), dts()],
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      name: "LernizTable",
-      fileName: "lerniz-table",
-      formats: ["es", "cjs", "umd"],
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
+export default defineConfig(({ mode }) => {
+  const isDev = mode !== "production"; // Obtiene el modo actual correctamente
+
+  return {
+    plugins: [vue(), ...(isDev ? [] : [dts()])], // Solo genera .d.ts en modo build
+    build: isDev
+      ? {} // Si está en modo dev, usa la configuración predeterminada de Vite
+      : {
+          lib: {
+            entry: "src/index.ts",
+            name: "LernizTable",
+            fileName: "lerniz-table",
+            formats: ["es", "cjs", "umd"],
+          },
+          rollupOptions: {
+            external: ["vue"],
+            output: {
+              globals: {
+                vue: "Vue",
+              },
+            },
+          },
         },
-      },
+    server: {
+      port: 3000,
     },
-  },
+  };
 });
